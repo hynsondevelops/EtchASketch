5eee0e4fc7236f6de93795f90c53fee33ea55405
Adam Hynson <hynsondevelops@gmail.com>
2016-12-12 12:27:33 -0500

    Grid Test

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function()
+{
+	//load grid
+	$h1 = $("<h1> Etch A Sketch </h1>");
+	$grid = $("<div id = 'grid'> </div>")
+	CreateGrid(5);
+});
+
+function CreateGrid(size) 
+{
+	for (i = 0; i < size; i++)
+	{
+		$(".grid").append("<div> class = 'unit'></div>");
+	}
+}
