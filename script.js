ae0c70c000f49de375fff7000214df4f15783959
Adam Hynson <hynsondevelops@gmail.com>
2016-12-12 16:25:37 -0500

    Test

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function()
 	//load grid woo
 	$h1 = $("<h1> Etch A Sketch </h1>");
 	$div = $("<div id = 'grid'> </div>");
-	$("body").append($div);
+	$("body").append($h1, $div);
 	CreateGrid(5);
 });
 
