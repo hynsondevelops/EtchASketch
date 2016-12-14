e31ab60b9566e6dd51b96179baff762d6d238927
Adam Hynson <hynsondevelops@gmail.com>
2016-12-14 11:58:35 -0500

    Still need to check if the user cancels out of the resize. Otherwise, done

--- script.js
+++ script.js
@@ -3,21 +3,42 @@ $(document).ready(function()
 	//load grid woo
 	$h1 = $("<h1> Etch A Sketch </h1>");
 	$div = $("<div id = 'grid'> </div>");
-	$("body").append($h1, $div);
-	CreateGrid(5);
-	ColorUnit();
+  $buttonReset = $("<button id = 'reset' type='button'>Reset</button>");
+  $buttonResize = $("<button id = 'resize' type = 'button'>Resize</button>");
+  $header = $("<header><h1> Etch A Sketch </h1> <p>Created by Adam Hynson</p> <button id = 'github' href = 'https://github.com/hynsondevelops'>Github</button></header>");
+	$("body").append($header, $div, $buttonReset, $buttonResize);
+	CreateGrid(10); //Default grid is 10x10
+  ResetGrid();
+  ResizeGrid();
 });
+
+function ResizeGrid()
+{
+	$('#resize').click(function()
+  {	
+  		var newSize = parseInt(prompt("Enter new grid size (up to 100x100)"));
+      while (newSize > 100 || newSize < 1)
+      {
+      	newSize = parseInt(prompt("Grid size invalid. Enter new grid size (up to 100x100)"));
+      }
+      $('.unit').remove();
+      CreateGrid(newSize);
+  })
+  return false;
+}
 
 function CreateGrid(size) 
 {
-	newGridSize = size * (parseInt($('.unit').height()) + parseInt($('.unit'.css('marginTop'))
-	$('#grid').height(newGridSize);
-	$('#grid').width(newGridSize);
 	$div = "<div class = 'unit'></div>";
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size*size; i++)
 	{
 		$("#grid").append($div);
 	}
+  newUnitSize = 500/size; //500 is size of grid
+	$('.unit').height(newUnitSize);
+	$('.unit').width(newUnitSize);
+  ColorUnit();
+  return false;
 }
 
 function ColorUnit()
@@ -26,4 +47,16 @@ function ColorUnit()
 	{
 		$(this).css('background-color', 'black');
 	})
+  return false;
 }
+
+function ResetGrid()
+{
+	$('#reset').click(function()
+  {
+			$('.unit').css('background-color', 'white');	  	
+  })
+  return false;
+}
+
+
